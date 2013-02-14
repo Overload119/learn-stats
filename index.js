9fd2cc3db80d354821493b5b8fc484f22ec18deb
Overload119 <budgeneration@gmail.com>
2013-02-13 21:50:13 -0500

    Getting things started off

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function() {
+  Reveal.initialize({
+    width: 960,
+    height: 700,
+    controls: false,
+    progress: true,
+    history: false,
+    keyboard: true,
+    overview: true,
+    center: true,
+    loop: false,
+    rtl: false,
+    autoSlide: 0,
+    mouseWheel: false,
+    rollingLinks: false,
+    transition: 'default' // default/cube/page/concave/zoom/linear/fade/none
+  });
+});
