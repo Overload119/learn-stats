4a33855b5dff643afbb0cf3253e91a88b6964cee
Overload119 <budgeneration@gmail.com>
2013-02-19 22:17:07 -0500

    Reworking things

--- index.js
+++ index.js
@@ -15,4 +15,20 @@ $(function() {
     rollingLinks: false,
     transition: 'default' // default/cube/page/concave/zoom/linear/fade/none
   });
+
+  $('#intro-btn').click(function() {
+    var currentStep = $(this).data('step');
+    console.log(currentStep);
+    if(currentStep === 1) {
+      Reveal.next();
+    } else {
+      $('.definition-text').fadeOut(500, function() {
+        $('.definition-text').text('If you\'re given a sample with a non-normal distribution, its sampling distribution of means will be approximately normal for large sample sizes (over 30).');
+        $('.definition-text').fadeIn();
+      });
+      $(this).val('I still don\'t get it');
+      $(this).data('step', 1);
+    }
+  });
+
 });
