e78612771f96c1201166544da7702b636554731f
nmost <nick@mostowich.com>
2013-02-23 17:31:37 -0500

    add card flip css/js, with graceful degradation using jquery animate

--- index.js
+++ index.js
@@ -30,6 +30,37 @@ $(function() {
       $(this).data('step', 1);
     }
   });
+
+  if ($('html').hasClass('csstransforms3d')) {    
+    // if it's supported, remove the scroll effect add the cool card flipping instead
+    $('.thumb').removeClass('scroll').addClass('flip');     
+
+    // add/remove flip class that make the transition effect
+    $('#user-graph-next-btn').click(
+      function () {
+        console.log('hit');
+        $('.thumb-wrapper').addClass('flipIt');
+      }/*,
+      function () {
+        $('.thumb-wrapper').removeClass('flipIt');           
+      }*/
+    );
+
+  } else {
+    // CSS 3D is not supported, use the scroll up effect instead
+    $('#user-graph-next-btn').click(
+      function () {
+        $('.thumb-detail').stop().animate({bottom:0}, 500);
+        $(this).click( function() {
+          //TODO: make next slide here
+        });
+      }/*,
+      function () {
+        $(this).find('.thumb-detail').stop().animate({bottom: ($(this).height() * -1) }, 500, 'easeOutCubic');          
+      }*/
+    );
+  }
+
   var userData = [ { x: 0, y: 3 },{ x: 1, y: 9 },{ x: 2, y: 7 },{ x: 3, y: 1 },{ x: 4, y: 6 }];
 
   var userDataGraph = new Rickshaw.Graph( {
@@ -38,15 +69,16 @@ $(function() {
       series: [
       {
         color: "#9c646b",
-        data: userData
+        data: userData,
+        name: 'Cuteness'
       }
       ]
   } );
 
   var hoverDetail = new Rickshaw.Graph.HoverDetail( {
         graph: userDataGraph,
-        xFormatter: function(x) { return "Cat " + (x+1) },
-        yFormatter: function(y) { return y + "/10 cuteness" }
+        xFormatter: function(x) { return null },
+        yFormatter: function(y) { return Math.floor(y) }
   } );
 
   userDataGraph.render();
