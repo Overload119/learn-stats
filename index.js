2b20d70fcb2fa7d76a48176caf1ffcc39b99af3e
Overload119 <budgeneration@gmail.com>
2013-03-02 12:12:58 -0500

    add cat images and code

--- index.js
+++ index.js
@@ -18,16 +18,33 @@ $(function() {
 
   $('#intro-btn').click(function() {
     var currentStep = $(this).data('step');
-    console.log(currentStep);
     if(currentStep === 1) {
       Reveal.next();
     } else {
-      $('.definition-text').fadeOut(500, function() {
+      $('.definition-text').fadeOut(250, function() {
         $('.definition-text').text('If you\'re given a sample with a non-normal distribution, its sampling distribution of means will be approximately normal for large sample sizes (over 30).');
-        $('.definition-text').fadeIn();
+        $('.definition-text').fadeIn(250);
       });
       $(this).val('I still don\'t get it');
       $(this).data('step', 1);
+    }
+  });
+
+  var catGameCounter = 1;
+  var catGameArray = [];
+  $('#play-game').click(function() {
+    $('.panel-two').fadeIn();
+    $(this).hide();
+  });
+  $('.panel-two').hide(); // Hide this at the start
+  $('.rate-button').click(function() {
+    var rating = $(this).val();
+    catGameArray.push(rating);
+    catGameCounter++;
+    $('.cat-image-container img').attr('src', './cats/cat' + catGameCounter + '.jpg');
+    $('.cat-image-container img').hide().fadeIn();
+    if(catGameArray.length === 5) {
+      Reveal.next();
     }
   });
 
